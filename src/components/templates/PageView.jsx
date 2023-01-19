b3539723e747139cd2aadcda1bf770d7d2f2f26a
fabiomagina <fabiomagina@gmail.com>
2023-01-19 11:05:00 -0300

    responsive adjustments

--- src/components/templates/PageView.jsx
+++ src/components/templates/PageView.jsx
@@ -2,19 +2,30 @@ import React, { useEffect, useState } from "react";
 import "./PageView.css"
 import Menu from "./Menu";
 import Dashboard from "../Dashboard";
+import { AiOutlineMenu } from 'react-icons/ai';
 
 const PageView = (props) => {
     const [dashboard, setDashboard] = useState(true)
+    const [menu, setMenu] = useState(false)
+    const [mobile, setMobile] = useState(false)
 
     useEffect(function () {
-        const dashboardview = document.querySelector('.view__dashboard')
+        const dashboardView = document.querySelector('.view__dashboard')
         if (dashboard) {
-            dashboardview.style.display = 'flex'
+            dashboardView.style.display = 'flex'
         } else {
-            dashboardview.style.display = 'none'
+            dashboardView.style.display = 'none'
         }
     }, [dashboard])
 
+    useEffect(function () {
+        const menuView = document.querySelector('.view__menu')
+        if (menu && mobile) {
+            menuView.setAttribute('style', 'z-index: 3')
+        } if (!menu && mobile) {
+            menuView.setAttribute('style', 'z-index: -1')
+        }
+    }, [menu, mobile])
 
     function handler(setter) {
         setter(current => !current)
@@ -22,14 +33,22 @@ const PageView = (props) => {
 
     return (
         <>
-            <div className="container__main">
+            <div className="row__menu--hamburguer" onClick={() => {
+                handler(setMenu)
+                setMobile(true)
+            }}>
+                <button className="menu--hamburguer" ><AiOutlineMenu /></button>
+            </div>
+            <div className="container__main" onClick={() => {
+                if (menu) handler(setMenu)
+            }}>
                 <div className="view__menu">
                     <Menu dashboardHandler={() => handler(setDashboard)} />
                 </div>
                 <div className="view__main">
                     <div className="template__main">
                         <div className="row__title">
-                        <h1 className="title">{props.title}</h1>
+                            <h1 className="title">{props.title}</h1>
                         </div>
                         {props.children}
                     </div>
