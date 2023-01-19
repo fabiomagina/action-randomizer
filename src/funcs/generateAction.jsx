b411fc79cb8508e26d792016328861d9899fc802
fabiomagina <fabiomagina@gmail.com>
2023-01-19 20:32:13 -0300

    functionalities fix

--- src/funcs/generateAction.jsx
+++ src/funcs/generateAction.jsx
@@ -3,28 +3,40 @@ import getRandomItem from './getRandomItem'
 import runRandomizer from './runRandomizer'
 
 export default async function generateMacro(nLoops, types, callback) {
+    let activeTypesIds = getActiveTypesIds(types)
     getActions()
-        .then(res => {
-            let activeTypes = getActiveTypes(types)
-            let newActions = ''
+        .then(actionsArray => {
+            let newMacro = ''
             for (let i = 0; i < nLoops; i++) {
-                newActions += newRandomizedActionsFromTypeId(res)
+                newMacro += generateActionsFromActiveTypes(actionsArray, activeTypesIds)
             }
-            return newActions
-            // return activeTypes
+            return newMacro
         })
         .then(callback)
 }
 
-function newRandomizedActionsFromTypeId(actions, idArray = [0]) {
-    let newMacro = ''
-    idArray.forEach(idArray => {
-        //adiciona um item da lista de cada tipo
-        newMacro += getRandomItem(actions[idArray].list)
-    })
-    return runRandomizer(newMacro)
+function generateActionsFromActiveTypes(actionsArray, activeTypesIds) {
+    let newActions = ''
+    newActions += newRandomizedActionsFromTypeId(actionsArray, activeTypesIds)
+
+    return newActions
 }
 
-function getActiveTypes(types) {
-    types.filter(type => type.status)
+function newRandomizedActionsFromTypeId(actionsArray, activeIds) {
+
+    let newMacro = ''
+    actionsArray.forEach(actionType => {
+        if (activeIds.includes(actionType.id)) {
+            console.log(actionType.id)
+            newMacro += runRandomizer(getRandomItem(actionType.list))
+        }
+    })
+    console.log(newMacro)
+    return newMacro
+}
+
+function getActiveTypesIds(types) {
+    let activeTypes = types.filter(type => type.status)
+    let activeTypesId = activeTypes.map(type => type.id)
+    return activeTypesId
 }
